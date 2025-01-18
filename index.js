0397ec12fe12ffd15a32c210acece732d3b25ac1
Sherry Yang <19577448+sherzyang@users.noreply.github.com>
2025-01-17 21:22:52 -0800

    Update.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    return customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = "george";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "henry";
+    return leastFavoriteCustomer; 
+}
